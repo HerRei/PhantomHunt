3e26f87174bd5f094f5936456913b5fe87080e47
Hermes Reisner <hermesnathanheiniger@gmail.com>
2026-02-25 18:54:20 -0500

    this adds a server with a echo function, pretty much in exaclty the way the slideset 05 ffrom fs25 explains it

---
+++ src/main/java/ch/unibas/dmi/dbis/cs108/example/server/ServerApp.java
@@ -0,0 +1,34 @@
+package ch.unibas.dmi.dbis.cs108.example.server;
+
+import ch.unibas.dmi.dbis.cs108.example.server.net.TcpServer;
+
+public final class ServerApp {
+
+    private static final int DEFAULT_PORT = 5050;
+
+    public static void main(String[] args) {
+        int port = parsePortOrDefault(args, DEFAULT_PORT);
+        System.out.println("SERVER starting on port " + port);
+
+        try {
+            TcpServer server = new TcpServer(port);
+            server.start();
+        } catch (Exception e) {
+            System.out.println("Server failed to start...");
+            e.printStackTrace();
+        }
+    }
+
+    private static int parsePortOrDefault(String[] args, int defaultPort) {
+        if (args == null || args.length == 0) return defaultPort;
+
+        try {
+            int p = Integer.parseInt(args[0]);
+            if (!(p >= 1 && p <= 65535)) throw new IllegalArgumentException("Not a valid port");
+            return p;
+        } catch (NumberFormatException e) {
+            System.out.println("Port was not a number, using default...");
+            return defaultPort;
+        }
+    }
+}
