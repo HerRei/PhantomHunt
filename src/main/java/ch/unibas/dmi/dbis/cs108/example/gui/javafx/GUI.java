2a82e3a21d36e1d3341968b52c1c139e06624138
Jan Haag <j.haag@stud.unibas.ch>
2026-03-13 09:46:20 +0100

    Added Class LoadingScene, and altered GUI override, modifications not complete jet.

--- src/main/java/ch/unibas/dmi/dbis/cs108/example/gui/javafx/GUI.java
+++ src/main/java/ch/unibas/dmi/dbis/cs108/example/gui/javafx/GUI.java
@@ -1,5 +1,6 @@
 package ch.unibas.dmi.dbis.cs108.example.gui.javafx;
 
+import ch.unibas.dmi.dbis.cs108.example.gui.javafx.scene.LoadingScene;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
@@ -7,28 +8,56 @@ import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 /**
- * This is a first JavaFX-Application, for the
- */
-
-/**
- * This is an example JavaFX-Application.
+ * This is a first JavaFX-Application, for the first Scene: LoadingScene
  */
 public class GUI extends Application {
+  private Stage stage;
+
+  public static void main(String[] args) {
+    launch(args);
+  }
 
   /**
+   * Sets up and opens the main game vidow
+   *
+   * @param stage
+   */
+  @Override
+  public void start(Stage stage) {
+    this.stage = stage;
+    this.stage.setTitle("Phantom Hunt");
+    //showLoadingScene();
+    this.stage.show();
+  }
+}
+
+  /**
+  public void showLoadingScene() {
+    LoadingScene loadingSceneBuilder = new LoadingScene();
+    Scene scene = loadingSceneBuilder.createScene();
+  }
+   */
+
+
+/**
+ * This is an example JavaFX-Application. - an Example Hello world code
+ *
+public class GUI extends Application {
+
+  **
    * Launching this method will not work on some platforms.
    * What you should do is to create a separate main class and launch the GUI class from there as is done in {@link Main}
-   */
+   *
   public static void main(String[] args) {
 
     launch(args);
   }
 
-  /**
+  **
    * Sets up and opens the main game window.
    *
    * @param stage
-   */
+   *
   @Override
   public void start(Stage stage) {
     String javaVersion = System.getProperty("java.version");
@@ -40,3 +69,4 @@ public class GUI extends Application {
   }
 
 }
+ */
