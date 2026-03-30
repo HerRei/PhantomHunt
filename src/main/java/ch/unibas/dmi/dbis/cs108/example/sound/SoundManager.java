802ed379351d3473cf85c338adcd1af451496142
Hermes Reisner <hermes.reisner@stud.unibas.ch>
2026-03-30 19:45:01 +0200

    This adds the placeholder for the SoundManager class that will need to implemented at some point

---
+++ src/main/java/ch/unibas/dmi/dbis/cs108/example/sound/SoundManager.java
@@ -0,0 +1,6 @@
+package ch.unibas.dmi.dbis.cs108.example.sound;
+
+public class SoundManager {}
+
+//this should be the high level class where sounds and sound effects should be played by.
+// e.g. playLobbyMusic() or footsteps(), to have a clean api and not needing to bother with the SoundEngine i made
