f7f42b70a357a39f1732b3e18c07db595648fc06
Hermes Reisner <hermes.reisner@stud.unibas.ch>
2026-04-25 11:33:35 +0200

    FIX: this removes the helloWorld placeholders form the inital commit

--- src/main/java/ch/unibas/dmi/dbis/cs108/phantomhunt/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-package ch.unibas.dmi.dbis.cs108.phantomhunt;
-
-/** A simple HelloWorld class. */
-public class HelloWorld {
-
-  public static void main(String[] args) {
-    System.out.println("Hello World");
-  }
-}
